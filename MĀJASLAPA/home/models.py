fa2140ecc50dc656e07fb6b8252981b0a924658c
Jkuznecovs <loh5576@gmail.com>
2024-04-24 17:42:38 +0300

    Add files via upload

--- MĀJASLAPA/home/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class Post(models.Model):
-    title= models.CharField(max_length=300)
-    text= models.TextField()
-    type= models.TextField()
-
